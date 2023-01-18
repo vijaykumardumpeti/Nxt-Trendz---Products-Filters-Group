f01559a4e50c9b6ae9cf19581d93639e4a2845da
VIJAYKUMAR <115571763+vijaykumardumpeti@users.noreply.github.com>
2023-01-18 16:58:16 +0530

    Update index.js

--- src/components/FiltersGroup/index.js
+++ src/components/FiltersGroup/index.js
@@ -6,24 +6,19 @@ const FiltersGroup = props => {
     categoryOptions,
     ratingsList,
     searchInputUpdation,
-    filterByCategory,
-    filterData,
+    categoryId,
+    ratingId,
+    setRatingFunction,
     titleSearch,
+    clearFilters,
   } = props // list
 
   const inputFunction = event => {
     searchInputUpdation(event)
   }
 
-  const searchButtonClicked = () => {
-    filterData()
-  }
-
-  const filterButtonClicked = () => {
-    // categoryOptions.map(eachCategory => filterByCategory(eachCategory.name))
-    categoryOptions.forEach(eachCategory => {
-      filterByCategory(eachCategory.name)
-    })
+  const clearFiltersFunction = () => {
+    clearFilters()
   }
 
   return (
@@ -32,50 +27,84 @@ const FiltersGroup = props => {
         <input
           value={titleSearch}
           onChange={inputFunction}
+          onKeyDown={inputFunction}
+          placeholder="Search"
           type="search"
           className="input-element"
         />
-        <button
-          onClick={searchButtonClicked}
-          type="button"
-          className="search-button"
-        >
+        <button type="button" className="search-button">
           <BiSearch className="search-icon" />
         </button>
       </div>
 
-      <p className="category">Category</p>
+      <h1 className="category">Category</h1>
 
       <ul className="filter-buttons-container">
-        {categoryOptions.map(eachObject => (
-          <li key={eachObject.categoryId}>
-            <button
-              onClick={filterButtonClicked}
-              className="button-stylings"
-              type="button"
-            >
-              {eachObject.name}
-            </button>
-          </li>
-        ))}
+        {categoryOptions.map(eachObject => {
+          const {filterByCategory} = props
+
+          const filterButtonClicked = () => {
+            filterByCategory(eachObject.categoryId)
+          }
+
+          const colorForButton =
+            categoryId === eachObject.categoryId ? 'apply-style' : ''
+
+          console.log(categoryId === eachObject.categoryId)
+
+          return (
+            <li key={eachObject.categoryId}>
+              <button
+                onClick={filterButtonClicked}
+                className={`button-stylings ${colorForButton}`}
+                type="button"
+              >
+                <p className="category-para"> {eachObject.name}</p>
+              </button>
+            </li>
+          )
+        })}
       </ul>
 
       <p className="category">Rating</p>
 
       <ul className="ratings-container">
-        {ratingsList.map(ratingObject => (
-          <li key={ratingObject.ratingId}>
-            <img
-              className="rating-image"
-              src={ratingObject.imageUrl}
-              alt={`rating ${ratingObject.ratingId}`}
-            />
-            & up
-          </li>
-        ))}
+        {ratingsList.map(ratingObject => {
+          //  console.log(ratingId)
+
+          const onSetTheRating = () => {
+            setRatingFunction(ratingObject.ratingId)
+          }
+
+          const ratingStyle =
+            ratingObject.ratingId === ratingId ? 'apply-style' : ''
+
+          return (
+            <li key={ratingObject.ratingId}>
+              <button
+                onClick={onSetTheRating}
+                type="button"
+                className="rating-container-button"
+              >
+                <div className="rating-single-container">
+                  <img
+                    className="rating-image"
+                    src={ratingObject.imageUrl}
+                    alt={`rating ${ratingObject.ratingId}`}
+                  />
+                  <p className={ratingStyle}>& up</p>
+                </div>
+              </button>
+            </li>
+          )
+        })}
       </ul>
 
-      <button className="clear-filter-button" type="button">
+      <button
+        onClick={clearFiltersFunction}
+        className="clear-filter-button"
+        type="button"
+      >
         Clear Filters
       </button>
     </div>
